e91866515a9c36bdd9faa7a58ddd4c252059a1dd
haunhph35954 <haunhph35954@fpt.edu.vn>
2025-06-22 15:35:45 +0700

    Hậu

--- src/main/java/com/example/datn_team_ae/Controller/Admin/AdminController.java
+++ src/main/java/com/example/datn_team_ae/Controller/Admin/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.datn_team_ae.Controller.Admin;
 
 
+import com.example.datn_team_ae.Service.KhachHangService;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    private KhachHangService khachHangService;
     @GetMapping
     public String index() {
         return "redirect:/admin/";
